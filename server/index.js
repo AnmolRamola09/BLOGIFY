dad9796b61438fd7c0d200e6a9c303fe42805e8a
AnmolRamola09 <anmolramola9@gmail.com>
2024-09-03 23:18:59 +0530

    new message

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const {
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({ withCredentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
