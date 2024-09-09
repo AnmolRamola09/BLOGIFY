ab730c778348ef971aefa3b1eec16aa8acd67185
AnmolRamola09 <anmolramola9@gmail.com>
2024-09-09 19:07:08 +0530

    committed newsletter

--- client/src/components/pages/home.jsx
+++ client/src/components/pages/home.jsx
@@ -72,10 +72,12 @@ const Home = () => {
           <div className="mt-6 flex w-full items-center space-x-2 md:w-1/3">
             <input
               className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-              type="text"
-              placeholder="Search Topics"
+              type="email"
+              value={newsLetter}
+              placeholder="E-mail"
             ></input>
             <button
+              onClick={handleSubscribe}
               type="button"
               className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
             >
