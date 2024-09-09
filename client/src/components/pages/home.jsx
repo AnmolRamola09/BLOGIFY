37f9970b6542d7a4a8016c8f179b8a36eb5ad594
AnmolRamola09 <anmolramola9@gmail.com>
2024-09-09 19:26:40 +0530

    committed newsletter

--- client/src/components/pages/home.jsx
+++ client/src/components/pages/home.jsx
@@ -71,6 +71,7 @@ const Home = () => {
           </p>
           <div className="mt-6 flex w-full items-center space-x-2 md:w-1/3">
             <input
+              onChange={(e) => setNewsLetter(e.target.value)}
               className="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
               type="email"
               value={newsLetter}
