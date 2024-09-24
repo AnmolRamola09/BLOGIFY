a6045e9a16659c08b43f00782f7f6ce9a655d52a
AnmolRamola09 <anmolramola9@gmail.com>
2024-09-24 11:58:46 +0530

    committed admin

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 const allowedOrigins = [
   "https://blogifyfrontend-anmol-ramolas-projects.vercel.app",
-  "https://blogify-admin-nu.vercel.app/",
+  "https://blogify-admin-nu.vercel.app",
 ];
 
 app.use(
